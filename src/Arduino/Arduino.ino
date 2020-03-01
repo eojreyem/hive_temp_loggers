1d58d78679b0793a255697423f43adeb2b46b4ac
eojreyem <joseph.c.meyer@gmail.com>
2020-02-29 22:12:37 -0600

    added button

--- src/Arduino/Arduino.ino
+++ src/Arduino/Arduino.ino
@@ -1,38 +1,52 @@
 /********************************************************************/
 // First we include the libraries
-#include <OneWire.h> 
+#include <OneWire.h>
 #include <DallasTemperature.h>
+#include <Library/Button.h>
 /********************************************************************/
-// Data wire is plugged into pin 2 on the Arduino 
-#define ONE_WIRE_BUS 2 
+// Data wire is plugged into pin 2 on the Arduino
+#define ONE_WIRE_BUS 2
+#define buttonPin 9
 /********************************************************************/
-// Setup a oneWire instance to communicate with any OneWire devices  
-// (not just Maxim/Dallas temperature ICs) 
-OneWire oneWire(ONE_WIRE_BUS); 
+// Setup a oneWire instance to communicate with any OneWire devices
+// (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
 /********************************************************************/
-// Pass our oneWire reference to Dallas Temperature. 
+// Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
-/********************************************************************/ 
-void setup(void) 
-{ 
- // start serial port 
- Serial.begin(9600); 
- Serial.println("Dallas Temperature IC Control Library Demo"); 
- // Start up the library 
- sensors.begin(); 
-} 
-void loop(void) 
-{ 
- // call sensors.requestTemperatures() to issue a global temperature 
- // request to all devices on the bus 
 /********************************************************************/
- Serial.print(" Requesting temperatures..."); 
- sensors.requestTemperatures(); // Send the command to get temperature readings 
- Serial.println("DONE"); 
+Button button;
+int mode = 0; // modes 0-battery status, 1-sampling, 2-hive statuses.
+int []
+
+
+void setup(void)
+{
+ // start serial port
+ Serial.begin(9600);
+ Serial.println("Dallas Temperature IC Control Library Demo");
+ // Start up the library
+ sensors.begin();
+
+ button.setup(buttonPin, [](int state) {
+    if (state == 1) {
+      Serial.println("button pressed")
+    }
+  });
+}
+void loop(void)
+{
+  button.update();
+ // call sensors.requestTemperatures() to issue a global temperature
+ // request to all devices on the bus
 /********************************************************************/
- Serial.print("Temperature is: "); 
- Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"?  
-   // You can have more than one DS18B20 on the same bus.  
-   // 0 refers to the first IC on the wire 
-   delay(1000); 
-} 
+ Serial.print(" Requesting temperatures...");
+ sensors.requestTemperatures(); // Send the command to get temperature readings
+ Serial.println("DONE");
+/********************************************************************/
+ Serial.print("Temperature is: ");
+ Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"?
+   // You can have more than one DS18B20 on the same bus.
+   // 0 refers to the first IC on the wire
+   delay(1000);
+}
